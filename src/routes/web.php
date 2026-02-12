a208d1c9c8d6afe04ed9c5e06bd5f16a409d914c
nishida namiki <ymdnmk1201@gmail.com>
2026-02-12 13:48:56 +0900

    不要なコードの削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -61,9 +61,7 @@ Route::middleware(['auth', 'verified', 'admin'])->prefix('admin')->name('admin.'
     Route::get('/attendance/list', [AdminAttendanceController::class, 'index'])->name('attendance.list');
     Route::get('/attendance/detail', [AdminAttendanceController::class, 'detail'])->name('attendance.detail');
     Route::post('/attendance/upsert', [AdminAttendanceController::class, 'upsert'])->name('attendance.upsert');
-    // Route::post('/attendance/save', [AdminAttendanceController::class, 'save'])->name('attendance.save');
     Route::get('/attendance/{id}', [AdminAttendanceController::class, 'show'])->name('attendance.show');
-    // Route::patch('/attendance/{id}', [AdminAttendanceController::class, 'update'])->name('attendance.update');
     Route::get('/staff/list', [AdminStaffController::class, 'index'])->name('staff.list');
     Route::get('/attendance/staff/{id}', [AdminStaffController::class, 'monthly'])->name('attendance.monthly');
 });
